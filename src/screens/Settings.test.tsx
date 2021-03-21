583d0cfd6a08d85fee79e7923528078b95d307e2
ChicK00o <hirohitthegreat@gmail.com>
2021-03-21 14:51:12 +0530

    removing redux

--- src/screens/Settings.test.tsx
+++ src/screens/Settings.test.tsx
@@ -1,15 +1,11 @@
 import { render } from '@testing-library/react-native';
 import React from 'react';
-import { Provider } from 'react-redux';
 import { Settings } from 'src/screens/Settings';
-import { store } from 'src/store';
 
 describe('Settings', () => {
   jest.useFakeTimers();
   const pageTree = (
-    <Provider store={store}>
       <Settings />
-    </Provider>
   );
 
   it('Should match snapshot', () => {
--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -1,3 +1,2 @@
-declare module 'redux-persist/lib/*';
 declare module 'reactotron-react-native/dist/flipper';
 declare module 'i18next-react-native-async-storage';
